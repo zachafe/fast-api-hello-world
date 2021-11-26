7beb911f041ed1fa559f3a78cfda14abec7e276e
afquintero <zachafe@gmail.com>
2021-11-26 17:00:17 -0500

    funcion de crear personas

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import  FastAPI
 
-
 app = FastAPI()
-
 
 @app.get("/")
 def home():
     return {"hello": "world"}
 
+# Request and Response Body
+
+@app.post("/person/new")
+def create_person():
+    pass
