bc94a4948018f850c9fe8c68292a7a414c4086ad
zachafe@gmail.com <zachafe@gmail.com>
2021-11-29 15:03:16 -0500

    validations_body

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ from fastapi import Body,Query,Path
 app = FastAPI()
 
 #Models
-
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+    
 class Person(BaseModel):
     first_name:str
     last_name:str
@@ -55,3 +59,20 @@ def show_person (
     person_id: int = Path (...,gt=0)
 ):
     return {person_id:"existe"}
+
+#validaciones: request Body
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title ="Person ID",
+        description ="This is the person Id",
+        gt = 0
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    
+    return results
